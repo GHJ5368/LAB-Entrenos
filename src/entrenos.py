000dedd8459bbaa24eebcd554390a114564dfa1b
GHJ5368 <jesantbar@alum.us.es>
2024-10-21 11:48:12 +0200

    modified:   src/entrenos.py modified:   src/entrenos_test.py

--- src/entrenos.py
+++ src/entrenos.py
@@ -1,16 +1,17 @@
-from datetime import *
+from datetime import datetime
 import csv   # Para leer los csv de datos
 from collections import namedtuple     # Para definir tuplas con nombre
 
 Entreno = namedtuple('Entreno', 'tipo, fechahora, ubicacion, duracion, calorias, distancia, frecuencia, compartido')
 
 def lee_entrenos(fichero):
-    entrenos=[]
+    entrenos = []
     with open(fichero,encoding="utf-8") as f:
         lector=csv.reader(f)
+        next(lector)
         for tipo, fechahora, ubicacion, duracion, calorias, distancia, frecuencia, compartido in lector:
             tipo = str(tipo)
-            fechahora = datetime(fechahora)
+            fechahora = datetime.strptime(fechahora, "%d/%m/%Y %H:%M")
             ubicacion =str(ubicacion)
             duracion =int(duracion)
             calorias =int(calorias)
@@ -21,3 +22,42 @@ def lee_entrenos(fichero):
             entrenos.append(entreno)
     return entrenos
 
+def tipos_enteros(entrenos):
+    '''
+
+    ########### OTRA FORMA ###########
+
+    res = set()
+    for entreno in entrenos:
+            res.add(entreno.tipo)
+
+    return sorted( res ) ## No hace falta pasar el set a lista
+
+    '''
+    
+    tipo_entrenos=[]
+    for entreno in entrenos:
+        if entreno.tipo not in tipo_entrenos:
+            tipo_entrenos.append(entreno.tipo)
+    
+    return sorted(tipo_entrenos)
+
+def entrenos_duracion_superior(entrenos,d):
+    res = []
+    for entreno in entrenos:
+        if entreno.duracion > d:
+            res.append(entreno)
+    
+    return res
+
+def sumar_calorias(entrenos,  f_inicio, f_fin):
+    #f_inicio = datetime.strptime(f_inicio, "%Y%m%d")
+    #f_fin = datetime.strptime(f_fin, "%Y%m%d")
+
+    res = 0
+    
+    for entreno in entrenos:
+        if f_inicio <=entreno.fechahora <= f_fin  :
+            res += entreno.calorias
+    
+    return res
--- src/entrenos_test.py
+++ src/entrenos_test.py
@@ -1,1 +1,30 @@
-from datetime import *
+
+from entrenos import *
+
+def test_lee_entrenos(fichero):
+    #print(f"Estamos probando la carga de datos de Entrenos")
+    entrenos = lee_entrenos(fichero)
+    #print(f"Tres primeros registros: {entrenos[:3]}")
+    #print(f"=======================================")
+    #print(f"Tres primeros registros: {entrenos[-3:]}")
+    return entrenos
+
+def test_tipos_enteros(entrenos):
+    tipos = tipos_enteros(entrenos)
+    print(f"Los tiposde entrenos en orden alfabetico son: {tipos}")
+
+def test_entrenos_duracion_superior(entrenos,d):
+    res = entrenos_duracion_superior(entrenos,d)
+    print(f"Los entrenos superiores a {d} son: {res}")
+    
+def test_sumar_calorias(entrenos,  f_inicio, f_fin):
+    res = sumar_calorias(entrenos,f_inicio, f_fin) 
+    print(f"La suma de calorias desde la fecha {f_inicio} hasta la fecha {f_fin} es: {res}")
+
+
+if __name__ == "__main__":
+    fichero = "LAB-Entrenos\data\entrenos.csv"
+    entrenos= test_lee_entrenos(fichero)
+    #test_tipos_enteros(entrenos)
+    #test_entrenos_duracion_superior(entrenos, 10)
+    test_sumar_calorias(entrenos, datetime(2020,12,1), datetime.now() )
